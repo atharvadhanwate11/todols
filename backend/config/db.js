f344052185907193eeffe4bb27a133b6d67108a7
ronak15 <ronakshah4415in@gmail.com>
2026-08-06 02:43:31 +0530

    created a db code

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,14 @@
-dfdfdf
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
